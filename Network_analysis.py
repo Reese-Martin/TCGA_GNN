817f60e55a2be24275535db718f90528c047b475
Reese_Martin <reese.a.martin@vanderbilt.edu>
2025-06-26 22:10:45 -0700

    filled out the subgraph section of the network_analysis script. now need to add saving for selected graphs and move to classification.

--- Network_analysis.py
+++ Network_analysis.py
@@ -20,17 +20,65 @@ MD = pd.read_csv('star_counts/DESeq2_metadata.csv', index_col=0)
 filtDF = normDF[[i for i in statsDF.index if statsDF.loc[i, 'padj'] < .05]]
 
 ### Identify subgraphs of importance in the network
-# This step could be done in any of a number of ways: modules from WGCNA, Louvain community detection, label propagation
-# by finding highly central genes, etc. For simplicity (and speed) I will proceed with label propagation.
-# if this ends up failing downstream I will likely revisit this step
+# This step could be done in any of a number of ways: modules from WGCNA, Louvain/leiden community detection,
+# label propagation by finding highly central genes, k-cliques, etc.
+# For computational simplicity, I will proceed using the basic process of identifying subgraphs in an adjacency matrix
+# based on the correlation matrix. From there collections of genes can be evaluated for their relationship to the
+# classification task (i.e. are all genes upregulated/downregulated, most significant/proportion of top 100
+# most significant genes, etc.). From there the analysis can hone in on just a few graphs for the classification task.
 
-# first generate a gene x gene correlation matrix and convert to absolute correlation (networkX label propagation does
-# not consider directed edges)
+# first generate a gene x gene correlation matrix, convert to absolute correlation
 corr_Mat = np.abs(np.corrcoef(filtDF.values, rowvar=False))
-# then convert to an adjacent, using the .7 threshold to identify strongly correlated genes as being connected
+
+# then convert to an adjacency matrix, using the .7 threshold to identify strongly correlated genes as being connected
 corr_Mat[corr_Mat > .7] = 1
 corr_Mat[corr_Mat < .7] = 0
 
 # convert adjacency matrix to networkx graph
 G = nx.convert_matrix.from_numpy_array(corr_Mat)
+cliques = nx.find_cliques(G)
 
+subgraphs = (G.subgraph(c) for c in connected_components)
+
+# discard the subgraphs with fewer than 5 members (this will be the majority of the subgraphs identified.)
+# I elected to keep the graphs with at least 5 connected members be
+subgraphs = [i for i in subgraphs if len(i.nodes) > 4]
+NodeCounts = [len(i) for i in subgraphs]
+print(f"{len(subgraphs)} subgraphs remaining after filtering")
+print(f"the largest graph is {max(NodeCounts)} genes and the median graph size is {np.median(NodeCounts)}")
+
+# Now we can filter our subgraphs based on the direction of their expression, their significance in DESeq2 (Note:
+# DESeq2 significance != biological significance)
+
+graphDF = pd.DataFrame(columns=['size', 'Prop.Upreg', 'Prop.Top1000LFC', 'Prop.Top1000padj'], index=['g_'+str(i) for i in range(1,len(subgraphs)+1)])
+absLFC = np.abs(statsDF['log2FoldChange'].values)
+absLFC.sort()
+LFCThresh = absLFC[-1000]
+
+padj = statsDF['padj'].values
+padj.sort()
+padjThresh = padj[1000]
+
+for gNum in range(len(subgraphs)):
+    ind = 'g_' + str(gNum+1)
+    graph = subgraphs[gNum]
+    g_genes = [normDF.columns.tolist()[i] for i in graph.nodes()]
+    graphDF.loc[ind, 'size'] = len(g_genes)
+    graphDF.loc[ind, 'Prop.Upreg'] = sum(statsDF.loc[g_genes]["log2FoldChange"].values > 0)/len(g_genes)
+    graphDF.loc[ind, 'Prop.Top1000LFC'] = (sum(np.abs(statsDF.loc[g_genes]["log2FoldChange"].values) > LFCThresh)/len(g_genes))
+    graphDF.loc[ind, 'Prop.Top1000padj'] = (sum(statsDF.loc[g_genes]["padj"].values > padjThresh)/len(g_genes))
+
+# explanations for the metrics I chose to track
+# size: important for limiting the graphs we take into the next step, as larger graphs will be more computationally
+# intensive
+# Prop.UpReg: here I am looking for genes that did the same thing ( all upregulated or all downregulated) indicating
+# regions of global up or down regulation
+# Prop.Top1000LFC: how many of the genes where in the top 1000 based on LFC, my thinking here is that genes that undergo
+# greater swings in expression will be easier for the models to get a good sense for
+# Prop.Top1000padj: a bit of a tie-breaker column, not intended to be the ultimate deciding factor, but could inform
+# the choice between close graphs.
+
+graphDF = graphDF.sort_values(by='Prop.Top1000LFC', ascending=False)
+graphDF.head()
+# based on the proportions in LFC and Prop.Upreg, g_9 and g_7 are promising, I will include g_1 as well because
+# it has an even mix between up and down regulated genes.
